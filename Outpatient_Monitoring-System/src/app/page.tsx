1a1c2d0cc54b941cf63f6bf4e54a3a89c0b920a8
khantNyarKoKo02 <6631503061@lamduan.mfu.ac.th>
2024-10-31 21:11:06 +0700

    test

--- Outpatient_Monitoring-System/src/app/page.tsx
+++ Outpatient_Monitoring-System/src/app/page.tsx
@@ -5,7 +5,7 @@ import { OutpatientTableProvider } from "./contexts/OutpatientTableContext";
 export default function Dashboard() {
   return (
     <div className=" bg-slate-200 overflow-y-scroll px-10 h-screen">
-      <h1 className="text-3xl font-semibold my-8 text-black">Dashboard</h1>
+      <h1 className="text-3xl font-semibold my-8 text-black">Dashboard Update</h1>
       <PatientSummary />
       <OutpatientTableProvider>
         <OutpatientTable />
